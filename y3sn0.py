c31f5a5c9c8a96eb3588ec167efe0f962c9f95dc
Tommy Nguyen <tommynguyen@tommys-mbp.lan>
2019-07-28 14:40:22 -0700

    target port 80 and remove debugging mode

--- y3sn0.py
+++ y3sn0.py
@@ -6,9 +6,11 @@ import os
 app = Flask(__name__)
 decision = ['yes','no']
 
+#create get method
 @app.route('/', methods=['GET'])
 def get():
     return jsonify({'msg':random.choice(decision)})
 
+#locally host on port 80
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0",)
+    app.run(host = "0.0.0.0", port=80)
